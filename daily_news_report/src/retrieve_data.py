7395bb1d918e8e4f1f283f32321d9e19c91ae71a
Matthias Schäfer <Matthias.Schaefer@itk-engineering.de>
2023-09-29 12:24:58 +0200

    first transfer from notebook to src folder and first pytest running

---
+++ daily_news_report/src/retrieve_data.py
@@ -0,0 +1,58 @@
+import requests
+import os
+import pprint
+from dotenv import load_dotenv
+from datetime import datetime, timedelta
+
+load_dotenv()
+
+
+class APICollector():
+    def __init__(self) -> None:
+        newsapi_news_collector = NEWSAPI_NewsCollector()
+
+        self.news_json = concatinate_news(
+            [newsapi_news_collector.get_news_json()])
+
+    def get_news_json(self):
+        return self.news_json
+
+
+class NEWSAPI_NewsCollector():
+    def __init__(self) -> None:
+        general_news = self._retreive_general_news()
+
+        self.news_json = concatinate_news([general_news])
+
+    def get_news_json(self):
+        return self.news_json
+
+    def _retreive_general_news(self):
+        url = 'https://newsapi.org/v2/everything?'
+        parameters = {
+            'q': '-Wetter OR -Musik OR -Verkehr OR -Fußball OR -Kommunen OR -heise+ OR -Spiel',  # query phrase
+            'pageSize': 10,  # maximum is 100
+            'apiKey': os.getenv("NEWSAPI_API_KEY"),  # your own API key
+            'sortBy': 'popularity',
+            # 'from': datetime.now().date(), 'to': datetime.now().date(),
+            'from': datetime.now().date()-timedelta(days=1), 'to': datetime.now().date(),
+            'language': 'de',
+            'domains': 'heise.de,stern.de,zeit.de,tagesschau.de',
+            'excludeDomains': 'welt.de,waz.de'
+        }
+        response = requests.get(url, params=parameters)
+
+        response_json = response.json()
+        pprint.pprint(response_json)
+
+        return response_json
+
+
+def retreive_news():
+    api_collector = APICollector()
+
+    return api_collector.get_news_json()
+
+
+def concatinate_news(json_list):
+    return json_list[0]  # first catch for only one news source
---
+++ tests/test_retrieve_data.py
@@ -0,0 +1,7 @@
+from daily_news_report.src.retrieve_data import retreive_news
+import pytest
+
+
+def test_function_runs() -> None:
+    news = retreive_news()
+    assert len(news) != 0
